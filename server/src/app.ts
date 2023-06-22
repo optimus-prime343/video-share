0e476da23caccc56bd8682501dbc425839acc11f
Sachin Yo <sachin_aryal>
2023-06-22 09:31:49 +0545

    Updated rate limit

--- server/src/app.ts
+++ server/src/app.ts
@@ -25,7 +25,7 @@ app.use(Express.static('public'))
 app.use(helmet())
 app.use(
   ratelimit({
-    windowMs: 15 * 60 * 1000 /* 15 minutes */,
+    windowMs: 60 * 1000 /* 1 minute */,
     max: 100 /* limit each IP to 100 requests per windowMs */,
   }),
 )
