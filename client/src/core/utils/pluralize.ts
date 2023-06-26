9222140279c372b9e8dd8611f51e40e72b726f54
Sachin Yo <sachin_aryal>
2023-06-26 16:58:07 +0545

    Updated pluralize

--- client/src/core/utils/pluralize.ts
+++ client/src/core/utils/pluralize.ts
@@ -4,4 +4,4 @@ a pluralized version of the word if the count is greater
 than 1, or the word itself otherwise.
 */
 export const pluralize = (word: string, count: number): string =>
-  count <= 1 ? word : `${word}s`
+  count === 1 ? word : `${word}s`
