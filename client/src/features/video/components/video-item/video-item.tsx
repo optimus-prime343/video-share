772c3250acb7eac066af7387a3078b5e57349d1e
Sachin Yo <sachin_aryal>
2023-07-18 13:12:11 +0545

    Refactored video item

--- client/src/features/video/components/video-item/video-item.tsx
+++ client/src/features/video/components/video-item/video-item.tsx
@@ -6,24 +6,24 @@ import type { Video } from '../../schemas/video'
 import { VideoItemInfo } from '../video-item-info'
 import { VideoItemThumbnail } from '../video-item-thumbnail'
 
-export interface ExperimentalVideoItemProps {
+export interface VideoItemProps {
   video: Video
   display?: 'row' | 'column'
 }
-export const VideoItem = ({ video, display = 'column' }: ExperimentalVideoItemProps) => {
-  const [showPlayer, setShowPlayer] = useState(false)
+export const VideoItem = ({ video, display = 'column' }: VideoItemProps) => {
+  const [hovered, setHovered] = useState(false)
 
   const { classes, cx } = useStyles()
   return (
     <Link
       className={cx(classes.root, classes[display])}
       href={{ pathname: '/watch', query: { id: video.id } }}
-      onMouseEnter={() => setShowPlayer(true)}
-      onMouseLeave={() => setShowPlayer(false)}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
     >
       <VideoItemThumbnail
         controls={display === 'column'}
-        showPlayerOnHover={showPlayer}
+        showPlayerOnHover={hovered}
         video={video}
       />
       <VideoItemInfo showAvatar={display === 'column'} video={video} />
